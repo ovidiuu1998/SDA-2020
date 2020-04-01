82d05b374ce4e1faa241f656ae77e39dc3250b97
Brasov Ovidiu <ovidiuamalia@yahoo.com>
2020-04-01 10:23:59 +0300

    asdg

--- lab1.1/src/prob3/main.java
+++ lab1.1/src/prob3/main.java
@@ -7,7 +7,7 @@ package prob3;
 			Adress address = new Adress("Ploiesti, ", "Domnisori, ", 11);
 			PaymentMethod pm = new PaymentMethod("card");
 			Customer customer = new Customer(address, pm);
-			Customer c1 = new Customer("Marina", "Coman ", 22);
+			Customer c1 = new Customer("Ovidiu", "Brasov ", 22);
 			System.out.println(c1.getCompleteName());
 			System.out.println(customer.getAdress().getFullAdress());
 			System.out.println(customer.getMethod().getType());
